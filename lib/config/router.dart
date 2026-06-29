f62d39459c4034d95980976b176285887f78b66c
fourment <clemfourment@hotmail.fr>
2026-06-29 02:22:18 +0200

    archi amelioration

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -2,19 +2,18 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'pages/onboarding_page.dart';
-import 'pages/login_page.dart';
-import 'pages/home_page.dart';
-
+import '../pages/onboarding_page.dart';
+import '../pages/login_page.dart';
+import '../pages/home_page.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
-  refreshListenable: GoRouterRefreshStream(FirebaseAuth.instance.authStateChanges()),
-  
+  refreshListenable:
+      GoRouterRefreshStream(FirebaseAuth.instance.authStateChanges()),
   redirect: (context, state) {
     final user = FirebaseAuth.instance.currentUser;
     final isLoggedIn = user != null;
-    
+
     final isGoingToLogin = state.matchedLocation == '/login';
     final isGoingToOnboarding = state.matchedLocation == '/';
 
@@ -28,13 +27,13 @@ final GoRouter appRouter = GoRouter(
 
     return null;
   },
-
   routes: [
     GoRoute(path: '/', builder: (context, state) => const OnboardingPage()),
     GoRoute(path: '/login', builder: (context, state) => const LoginPage()),
     GoRoute(path: '/home', builder: (context, state) => const HomePage()),
   ],
 );
+
 class GoRouterRefreshStream extends ChangeNotifier {
   GoRouterRefreshStream(Stream<dynamic> stream) {
     notifyListeners();
